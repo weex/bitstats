7082482b61d971fe484de563b8019d80a535352d
David Sterry <davids@exchb.com>
2017-09-17 06:55:11 +0000

    put data into mysql

--- tail_bandwidth.py
+++ tail_bandwidth.py
@@ -2,9 +2,17 @@
 import time
 import subprocess
 import select
+import mysql.connector
+
+from settings import *
+
+con=mysql.connector.connect(user=MYSQL_USER,
+                            password=MYSQL_PASSWORD,
+                            database=MYSQL_DATABASE)
+cur=con.cursor()
 
 filename = '/var/log/bandwidth'
-
+seconds = 5
 ports = ['30303', '8333']
 
 f = subprocess.Popen(['tail','-F',filename],\
@@ -13,7 +21,7 @@ proc = select.poll()
 proc.register(f.stdout)
 
 
-def g():
+def process():
     start = time.time()
     kBps = {}
     for p in ports:
@@ -26,20 +34,25 @@ def g():
             data = parse(f.stdout.readline())
             Bytes = int(data['LEN'])
             for p in ports:
-                
                 if data.get('SPT', '') == p or data.get('DPT', '') == p:
                     kBps[p]['tot'] += Bytes
                     if data.get('DIR', '') == 'IN':
                         kBps[p]['in'] += Bytes
                     else:
                         kBps[p]['out'] += Bytes
-            if time.time() - start > 1:
+            if time.time() - start > seconds:
                 print kBps
+                for k in kBps:
+                    for metric in kBps[k]:
+                        insertstmt=("insert into bandwidth (created, metric, value) values (now(), '%s', '%s')" % (k+'_'+metric, kBps[k][metric]/float(seconds)))
+                        cur.execute(insertstmt)
+                    con.commit()
                 for p in ports:
                     kBps[p]['tot'] = 0
                     kBps[p]['in'] = 0
                     kBps[p]['out'] = 0
                 start = time.time()
+    con.close()
 
 #  Sep 17 03:42:01 usloft5264 kernel: [347006.315335] BANDWIDTH_IN:IN=eth0 OUT= MAC=84:34:97:11:3b:bc:00:24:38:a4:b8:00:08:00 SRC=163.44.174.122 DST=209.239.115.243 LEN=52 TOS=0x00 PREC=0x00 TTL=50 ID=40038 DF PROTO=TCP SPT=53482 DPT=30303 WINDOW=10077 RES=0x00 ACK URGP=0
 
@@ -66,10 +79,5 @@ def parse(line):
 
 
 if __name__ == "__main__":
-    g()
+    process()
 
-
-#while True:
-#    if p.poll(1):
-#        print f.stdout.read()
-#    time.sleep(1)

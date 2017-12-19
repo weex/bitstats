c7a3bb2a6395973bf188d82d8ae09faf6efdbcd7
David Sterry <davids@exchb.com>
2017-12-19 00:15:05 +0000

    use Popen to talk to bitcoind

--- blocks.py
+++ blocks.py
@@ -1,4 +1,5 @@
-import subprocess
+#import subprocess
+from subprocess import Popen, PIPE, STDOUT;
 import json
 import mysql.connector
 
@@ -9,8 +10,16 @@ con=mysql.connector.connect(user=MYSQL_USER,
                             database=MYSQL_DATABASE)
 cur=con.cursor()
 
-out = subprocess.check_output(['bitcoin-cli', 'getinfo'])
-parsed = json.loads(out)
+def get_info():
+                command = ['bitcoin-cli']
+                command.extend(['getinfo'])
+                p = Popen(command, stdout=PIPE)
+                io = p.communicate()[0]
+                return json.loads(io)
+
+
+#out = subprocess.check_output(['bitcoin-cli', 'getinfo'])
+parsed = get_info() # json.loads(out)
 insertstmt=("insert into proc (created, metric, value) values (now(), '%s', '%s')" % ('blocks_bitcoind', parsed['blocks']))
 cur.execute(insertstmt)
 insertstmt=("insert into proc (created, metric, value) values (now(), '%s', '%s')" % ('difficulty_bitcoind', parsed['difficulty']))
